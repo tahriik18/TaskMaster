1568f8f76b6f21856c41ecccacf31c5c293f805a
Naery Kouyoumjian <naery.kouyoumjian.750@my.csun.edu>
2025-03-31 08:52:16 -0700

    Added client folder with React setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TaskMaster</h1>
+      <p>Welcome to your to-do list application!</p>
+    </div>
+  );
+}
+
+export default App;
